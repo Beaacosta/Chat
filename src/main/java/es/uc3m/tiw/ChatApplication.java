102ffca96ce674fda82efd7b0095b9db16c0b6a9
Beaacosta <100304636@alumnos.uc3m.es>
2016-12-01 15:35:39 +0000

    conexion con mysql

--- src/main/java/es/uc3m/tiw/ChatApplication.java
+++ src/main/java/es/uc3m/tiw/ChatApplication.java
@@ -2,11 +2,14 @@ package es.uc3m.tiw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ChatApplication {
+public class ChatApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChatApplication.class, args);
 	}
+
 }
